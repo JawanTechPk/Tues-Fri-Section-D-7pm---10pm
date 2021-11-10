aa867f52962e5234659c5def02fb86fbe381643c
jawantechpk <jawantechpk@gmail.com>
2021-11-10 20:11:26 +0500

    update
    update

---
+++ class 09-11-2021/app.js
@@ -0,0 +1,46 @@
+// var arr = ["BSC","ATM"];
+
+// for(var i =0;i<arr.length;i++){
+//     document.write(i+")"+arr[i]+"<br />")
+// }
+
+
+
+// var currentTime = new Date();
+
+// console.log(currentTime);
+
+
+// var currentTime = new Date();
+
+// console.log(currentTime.toString());
+
+
+// var currentTime = new Date();
+
+// console.log(currentTime.getDate());
+
+
+// var currentTime = new Date();
+
+// console.log(currentTime.getTime());
+
+
+// var doomsDay = new Date("June 30,2035");
+
+// console.log(doomsDay.getTime());
+
+// var msDiff = doomsDay.getTime() - currentTime.getTime();
+
+// console.log('msDiff',msDiff);
+
+// var result = msDiff / ( 1000*60*60*24 );
+// console.log(result,'days')
+
+
+
+
+// var currentTime = new Date("2007");
+
+
+// console.log(currentTime);
---
+++ class 25 05-11-2021/app.js
@@ -0,0 +1,106 @@
+// var rates = 4.8343;
+
+// console.log(Math.round(rates));
+
+
+// -3  -2   -1   0   1     2    3
+
+
+// -3 -2.9 -2.8 -2.7 -2.6 -2.5 -2.4 -2.3 -2.2  -2.1  -2
+
+
+// 2   2.1  2.2  2.3  2.4  2.5  2.6  2.7  2.8  2.9    3
+
+// var rates = -2.5343;
+
+// console.log(Math.round(rates));
+
+
+
+// var rates = 2.120360;
+
+// console.log(Math.ceil(rates));
+
+
+// -3  -2   -1   0   1     2    3
+
+
+// -3 -2.9 -2.8 -2.7 -2.6 -2.5 -2.4 -2.3 -2.2  -2.1  -2
+
+
+// 2   2.1  2.2  2.3  2.4  2.5  2.6  2.7  2.8  2.9    3
+
+
+// var rates = -2.120360;
+
+// console.log(Math.floor(rates));
+
+
+
+// console.log(Math.random() * 6);
+
+
+// var num = Math.random();
+
+// var numUpdate = (num*50) + 1;
+
+// var result = Math.floor(numUpdate);
+
+// console.log(result);
+
+
+
+
+
+// var num = "200" - "50";
+// var num1 = "200" - 50;
+// var num2 = 200 - 50;
+
+
+// console.log(num , num1 , num2)
+
+
+// var num = "200" - "khan";
+
+
+
+// console.log(num )
+
+
+
+var num = "200.87988";
+
+
+num = parseInt(num)
+
+console.log( num )
+
+
+
+// var num = "200.87988";
+
+
+// num = Number(num)
+
+// console.log( num )
+
+
+
+// var num = 200.87988;
+
+
+// num = num.toString()
+
+// console.log(typeof num )
+
+
+
+// var num = 200.87988;
+
+
+// num = num.toFixed(1)
+
+// console.log( num )
+
+
+
